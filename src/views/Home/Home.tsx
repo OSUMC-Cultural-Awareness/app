66f4cc232eb5862778f9ab2948b1c26b0d4a7545
Nick LaManna <nick.lamanna@ibm.com>
2020-12-02 19:19:13 -0500

    Try Catch Around Ledger.list

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -55,137 +55,138 @@ function Home(props: Props): React.ReactElement {
       setCultures(cultures);
     } catch (err) {
       if (err instanceof OfflineError) {
-        const ledger = await Ledger.list();
-        let cultures = [];
-        ledger.forEach(
-          (val, key) => cultures.push({ name: key, modified: cultures[key] })
-        );
-        setCultures(cultures)
-        setOffline(true)
-      } else {
-        setMsg(err.toString())
+        try {
+          const ledger = await Ledger.list();
+          let cultures = [];
+          ledger.forEach(
+            (val, key) => cultures.push({ name: key, modified: cultures[key] })
+          );
+          setCultures(cultures)
+          setOffline(true)
+        } catch (err) {
+          setMsg(err.toString())
+        }
       }
+    };
+
+    useEffect(() => {
+      fetchCultures();
+    }, []);
+
+    const fetchAdmins = async () => {
+      if (!props.token) {
+        return;
+      }
+
+      const admins = user.superUser ? await Admin.list(token) : [user];
+      setAdmins(admins);
+    };
+
+    useEffect(() => {
+      fetchAdmins();
+    }, []);
+
+    if (!token) {
+      return (
+        <Cultures
+          navigation={props.navigation}
+          token={""}
+          cultures={cultures}
+          onRefresh={() => fetchCultures()}
+          offline={offline}
+        />
+      );
     }
-  };
 
-  useEffect(() => {
-    fetchCultures();
-  }, []);
+    const onAdd = () => {
+      switch (getFocusedRouteNameFromRoute(route) ?? "Cultures") {
+        case "Cultures":
+          setCultures([
+            ...cultures,
+            { name: "New Culture", modified: Date.now() },
+          ]);
+          break;
+        case "Admins":
+          setInviteModal(true);
+      }
+    };
 
-  const fetchAdmins = async () => {
-    if (!props.token) {
-      return;
+    const onInvite = async (email: string) => {
+      try {
+        await Admin.invite(email, token);
+      } catch (err) {
+        // show error message
+      }
+    };
+
+    if (!admins) {
+      return (
+        <ActivityIndicator animating={true} size="large" style={styles.spinner} />
+      );
     }
 
-    const admins = user.superUser ? await Admin.list(token) : [user];
-    setAdmins(admins);
-  };
+    const fabStyles = {
+      position: Platform.OS === "web" ? "fixed" : "absolute",
+      margin: 16,
+      right: 0,
+      bottom: 0,
+    };
 
-  useEffect(() => {
-    fetchAdmins();
-  }, []);
-
-  if (!token) {
     return (
-      <Cultures
-        navigation={props.navigation}
-        token={""}
-        cultures={cultures}
-        onRefresh={() => fetchCultures()}
-        offline={offline}
-      />
+      <View style={styles.view}>
+        <Tab.Navigator initialRouteName="Cultures">
+          <Tab.Screen name="Cultures">
+            {() => (
+              <Cultures
+                navigation={navigation}
+                token={token}
+                cultures={cultures}
+                onRefresh={() => fetchCultures()}
+                offline={offline}
+              />
+            )}
+          </Tab.Screen>
+          <Tab.Screen name="Admins">
+            {() => (
+              <Admins
+                token={token}
+                admins={admins}
+                onRefresh={() => fetchAdmins()}
+              />
+            )}
+          </Tab.Screen>
+        </Tab.Navigator>
+        <FAB icon="plus" style={fabStyles as any} onPress={onAdd} />
+        <Portal>
+          <Snackbar
+            visible={msg != ""}
+            onDismiss={() => setMsg("")}
+            action={{
+              label: 'Undo', onPress: () =>
+                setMsg("")
+            }}>
+            {msg}
+          </Snackbar>
+          <Modal visible={inviteModal} onDismiss={() => setInviteModal(false)}>
+            <Text>Example Modal. Click outside this area to dismiss.</Text>
+          </Modal>
+        </Portal>
+      </View>
     );
   }
 
-  const onAdd = () => {
-    switch (getFocusedRouteNameFromRoute(route) ?? "Cultures") {
-      case "Cultures":
-        setCultures([
-          ...cultures,
-          { name: "New Culture", modified: Date.now() },
-        ]);
-        break;
-      case "Admins":
-        setInviteModal(true);
-    }
-  };
-
-  const onInvite = async (email: string) => {
-    try {
-      await Admin.invite(email, token);
-    } catch (err) {
-      // show error message
-    }
-  };
-
-  if (!admins) {
-    return (
-      <ActivityIndicator animating={true} size="large" style={styles.spinner} />
-    );
-  }
-
-  const fabStyles = {
-    position: Platform.OS === "web" ? "fixed" : "absolute",
-    margin: 16,
-    right: 0,
-    bottom: 0,
-  };
-
-  return (
-    <View style={styles.view}>
-      <Tab.Navigator initialRouteName="Cultures">
-        <Tab.Screen name="Cultures">
-          {() => (
-            <Cultures
-              navigation={navigation}
-              token={token}
-              cultures={cultures}
-              onRefresh={() => fetchCultures()}
-              offline={offline}
-            />
-          )}
-        </Tab.Screen>
-        <Tab.Screen name="Admins">
-          {() => (
-            <Admins
-              token={token}
-              admins={admins}
-              onRefresh={() => fetchAdmins()}
-            />
-          )}
-        </Tab.Screen>
-      </Tab.Navigator>
-      <FAB icon="plus" style={fabStyles as any} onPress={onAdd} />
-      <Portal>
-        <Snackbar
-          visible={msg != ""}
-          onDismiss={() => setMsg("")}
-          action={{
-            label: 'Undo', onPress: () =>
-              setMsg("")
-          }}>
-          {msg}
-        </Snackbar>
-        <Modal visible={inviteModal} onDismiss={() => setInviteModal(false)}>
-          <Text>Example Modal. Click outside this area to dismiss.</Text>
-        </Modal>
-      </Portal>
-    </View>
-  );
-}
-
-export default connect(
-  (
-    state: Store,
-    ownProps: {
-      navigation: StackNavigationProp<Routes, "Home">;
-      route: RouteProp<Routes, "Home">;
-    }
-  ) => ({
-    navigation: ownProps.navigation,
-    route: ownProps.route,
-    user: state.user.user,
-    token: state.user.token,
-  }),
-  null
-)(Home);
+  export default connect(
+    (
+      state: Store,
+      ownProps: {
+        navigation: StackNavigationProp<Routes, "Home">;
+        route: RouteProp<Routes, "Home">;
+      }
+    ) => ({
+      navigation: ownProps.navigation,
+      route: ownProps.route,
+      user: state.user.user,
+      token: state.user.token,
+    }),
+    null
+  )(Home);
