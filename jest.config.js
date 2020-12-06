980143f0f60fbbaba143d14d95bedc8a5824fcf1
Nick Hackman <snickhackman@gmail.com>
2020-12-06 03:23:25 -0500

    Tests: Setup Jest w/ Enzyme and Typescript
    Includes mocking for React Navigation, AsyncStorage, Redux, and more.

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  preset: "jest-expo",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.tsx?$": "babel-jest",
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)",
+  ],
+  setupFiles: ["./jest.setup.js"],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "android.ts",
+    "android.tsx",
+  ],
+  setupFilesAfterEnv: ["<rootDir>setup.tests.js"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{ts,tsx,js,jsx}",
+    "!**/coverage/**",
+    "!**/node_modules/**",
+    "!**/babel.config.js",
+    "!**/jest.setup.js",
+    "!**/setup.tests.js",
+    "!**/jest.config.js",
+    "!**/setup-envs.js",
+  ],
+};
+
